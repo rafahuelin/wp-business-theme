25f37b71fc21d83daaf890975e99d684411cbc91
Rafa Huelin <it@engisa.com>
2017-07-27 18:17:44 +0100

    Create theme widget positions

--- wp-content/themes/business-theme/footer.php
+++ wp-content/themes/business-theme/footer.php
@@ -2,35 +2,21 @@
 <footer class="footer content-region-3 pt30 pb40">
     <div class="container">
         <div class="col-lg-4">
-            <h4>Heading</h4>
-            <p>Curabitur condimentum malesuada sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas condimentum, risus ac euismod efficitur.</p>
+            <?php if(is_active_sidebar('footer-1')) : ?>
+                <?php dynamic_sidebar('footer-1') ?>
+            <?php endif; ?>
         </div>
 
         <div class="col-lg-4">
-            <h4>Heading</h4>
-            <ul>
-                <li><a href="#">Condimentum aliquam</a></li>
-                <li><a href="#">Hendrerit magna</a></li>
-                <li><a href="#">Suspendisse potenti</a></li>
-                <li><a href="#">Praesent fringilla</a></li>
-                <li><a href="#">Donec efficitur</a></li>
-            </ul>
+            <?php if(is_active_sidebar('footer-2')) : ?>
+                <?php dynamic_sidebar('footer-2') ?>
+            <?php endif; ?>
         </div>
 
         <div class="col-lg-4">
-            <h4>Contact Us</h4>
-            <form>
-                <div>
-                    <input type="text" placeholder="Name">
-                </div>
-                <div>
-                    <input type="email" placeholder="Email">
-                </div>
-                <div>
-                    <textarea placeholder="Message"></textarea>
-                </div>
-                <input class="btn btn-primary" type="submit" value="Send">
-            </form>
+            <?php if(is_active_sidebar('footer-3')) : ?>
+                <?php dynamic_sidebar('footer-3') ?>
+            <?php endif; ?>
         </div>
     </div>
 </footer>
--- wp-content/themes/business-theme/front-page.php
+++ wp-content/themes/business-theme/front-page.php
@@ -36,29 +36,16 @@
     </div>
 </section>
 
-<section class="row content-region-1 pt50 pb40">
-    <div class="container">
-        <div class="col-md-12">
-            <h1>Clean Bootstrap Wordpress Theme</h1>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam finibus ullamcorper diam, vel laoreet sapien placerat at. Nam lacinia tellus nunc.</p>
-        </div>
-    </div>
-</section>
+<!--Widget content-region-1-->
+<?php if(is_active_sidebar('content-region-1')) : ?>
+    <?php dynamic_sidebar('content-region-1'); ?>
+<?php endif; ?>
 
-<section class="row content-region-2 pt40 pb40">
-    <div class="container">
-        <div class="col-md-5">
-            <img src="img/pic1.jpg">
-        </div>
-        <div class="col-md-7">
-            <h3>Theme Features</h3>
-            <ul class="list-group">
-                <li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Clean Code</li>
-                <li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Custom Showcase Area</li>
-                <li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Bootstrap 3 Framework</li>
-                <li class="list-group-item"><i class="fa fa-check" aria-hidden="true"></i> Multiple Color Choices</li>
-            </ul>
-        </div>
+<!--Widget content-region-2-->
+<?php if(is_active_sidebar('content-region-2')) : ?>
+    <?php dynamic_sidebar('content-region-2'); ?>
+<?php endif; ?>
+
     </div>
 </section>
 <?php get_footer(); ?>
